a63d0cd17b8a4d690949fea096ce65ef0d274ed4
vtu29735-bit <vtu29735@veltech.edu.in>
2026-08-06 13:07:53 +0530

    Create week2.find the highest altitude.29735.java

---
+++ week2.find the highest altitude.29735.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int altitude = 0;
+        int maxAltitude = 0;
+        
+        for (int g : gain) {
+            altitude += g;
+            maxAltitude = Math.max(maxAltitude, altitude);
+        }
+        
+        return maxAltitude;
+    }
+}
+output:
+gain =
+[-5,1,5,0,-7]
